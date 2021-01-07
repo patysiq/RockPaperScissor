d626de545a3bb999edcfcfc1c03bd1bb257a060d
Patricia <patysiq@yahoo.com.br>
2021-01-07 15:41:43 -0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by PATRICIA S SIQUEIRA on 07/01/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
